e9e0c11b567c0c72ed05f84808fdf63d9d853a0a
TirthCodez <tirthpopat1722@gmail.com>
2024-03-01 00:58:33 +0530

    Program to print given pattern-1

--- Program4.py
+++ Program4.py
@@ -10,12 +10,23 @@
 *
 '''
 x=int(input("Enter the number:"))
-for i in range(1,2):
-    for i in range(1,x+1):
-        for j in range(1,i+1):
-            print("*",end=" ")
-        print(" ")
-    for i in range(x-1,0,-1):
-        for j in range(1,i+1):
-            print("*",end=" ")
-        print(" ")
+'''runs code from 1 to the number user defined like if user gives 3 it will print
+*
+**
+***
+'''
+for i in range(1,x+1): 
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print(" ")
+'''This other for loop is used to print the below pattern after what is already printed
+Taking one number less than what user give hence not repeating the last pattern
+The output of this will be if user have given x=3
+**
+*
+'''
+for i in range(x-1,0,-1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print(" ")
+    
